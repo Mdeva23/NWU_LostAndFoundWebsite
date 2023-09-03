b25d64f2831f06fb9f5d3814bc9907e91304eba3
SSD SEDUMEDI <38436272@mynwu.ac.za>
2023-09-04 00:36:13 +0200

    Code to add new users into the database

--- NWU_LostAndFoundWebsite/CreateAccount.aspx.cs
+++ NWU_LostAndFoundWebsite/CreateAccount.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace NWU_LostAndFoundWebsite
 {
@@ -14,22 +15,49 @@ namespace NWU_LostAndFoundWebsite
 
         }
 
+        string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\David\Dropbox\PC\Desktop\CMPG223\NWU_LostAndFoundWebsite\NWU_LostAndFoundWebsite\NWU_LostAndFoundWebsite\App_Data\LostAndFound.mdf;Integrated Security=True";
+        SqlCommand command;
+        SqlConnection con;
+        SqlDataAdapter adapter;
+        SqlDataReader dataReader;
+
+
         protected void btnCreateAccount_Click(object sender, EventArgs e)
         {
-            String name = txtName.Text;
-            String surname = txtSurname.Text;
-            String email = txtEmail.Text;
-            String contact = txtContact.Text;
-            String password = txtPassword.Text;
-            String reEnterPassword = txtReEnterPass.Text;
-
-            if(password == reEnterPassword)
+            try
             {
-                lblPassNotMatch.Text = "The passwords entered do not match. Please re enter the passowrd";
+                //DECLARING THE VARIABLES REQUIRED TO CREATE ACCOUNT
+                String name = txtName.Text;
+                String surname = txtSurname.Text;
+                String email = txtEmail.Text;
+                String contact = txtContact.Text;
+                String password = txtPassword.Text;
+                String reEnterPassword = txtReEnterPass.Text;
+
+                if(password == reEnterPassword)    //CHECKING IF THE PASSWORDS MATCH
+                {
+                    using(con = new SqlConnection(conString))
+                    {
+                        con.Open();      //OPENING THE SQL CONNECTION
+                        command = new SqlCommand("INSERT INTO tblUsers(userID, userName, userSurname, userEmail, userContact) VALUES(@id, @name, @surname, @email, @contact)", con);   //INSERTING VALUES INTO THE TABLES
+                        command.Parameters.AddWithValue("@id", "Value");
+                        command.Parameters.AddWithValue("@name", name);
+                        command.Parameters.AddWithValue("@surname", surname);
+                        command.Parameters.AddWithValue("@email", email);
+                        command.Parameters.AddWithValue("@contact", contact);
+                        command.ExecuteNonQuery();
+
+                        con.Close();      //CLOSING THE CONNECTION
+                    }
+                }
+                else
+                {
+                    lblPassNotMatch.Text = "The passwords entered do not match. Please try again";  //MESSAGE TO MAKE USER AWARE OF DIFFERENT PASSWORDS
+                }
             }
-            else
+            catch(Exception ex)
             {
-
+                Console.WriteLine(ex.Message);
             }
         }
     }
